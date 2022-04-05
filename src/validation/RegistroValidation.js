866ea53d4c8b64a7768e18050fcdd3a1c0c87a07
gbp12 <gonzabonilla4896@outlook.es>
2022-04-05 13:11:59 +0100

    errores modelados

--- src/validation/RegistroValidation.js
+++ src/validation/RegistroValidation.js
@@ -1,8 +1,17 @@
 import * as yup from "yup"
 
 export const validation = yup.object().shape({
-	nombre: yup.string().min(3, "el nombre debe tener mas de 3 letras").max(15, "el nombre debe tener menos de 15 letras"),
-	apellido: yup.string().min(3, "el apellido debe tener mas de 5 letras").max(15, "el apellido debe tener menos de 15 letras"),
-	email: yup.string().min(8, "el email debe tener mas de 8 letras"),
-	direccion: yup.string().min(3, "tu dirección debe tener mas de 3 letras").max(50, "el nombre debe tener menos de 50 letras"),
+	nombre: yup
+		.string()
+		.min(3, <p style={{ color: "red" }}>el nombre debe tener mas de 3 letras</p>)
+		.max(15, <p style={{ color: "red" }}>el nombre debe tener menos de 15 letras</p>),
+	apellido: yup
+		.string()
+		.min(5, <p style={{ color: "red" }}>el apellido debe tener mas de 5 letras</p>)
+		.max(15, <p style={{ color: "red" }}>el nombre debe tener menos de 3 15letras</p>),
+	email: yup.string().min(8, <p style={{ color: "red" }}>tu email debe tener mas de 8 letras</p>),
+	direccion: yup
+		.string()
+		.min(10, <p style={{ color: "red" }}>tu direccion debe tener mas de 10 caracteres</p>)
+		.max(50, <p style={{ color: "red" }}>tu direccion debe tener menos de 50 caracteres</p>),
 })
